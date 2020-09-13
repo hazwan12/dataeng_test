a830da736c5ee6142c8652bb2b5706f5183e1a69
hazwan12 <36067775+hazwan12@users.noreply.github.com>
2020-09-13 16:51:43 +0800

    Adding split to name for fname and lname

--- main.py
+++ main.py
@@ -14,9 +14,21 @@ def read_file(file_name : str, file_sep : str = ",", file_header : int = 0):
     print("File read successfully with counts {}".format(len(df)))
     return df
 
+def split_name(df : pandas.DataFrame):
+
+
+    if "name" in df.columns:
+        df[["firstname", "lastname"]] = df["name"].str.split(pat = " ", n = 1, expand = True)
+        return df
+    else:
+        raise("Column 'name' not in dataframe")
+
 if __name__ == "__main__" :
     try:
         df = read_file("dataset.csv")
+        print(df)
+
+        df = split_name(df)
         print(df)
     except Exception as e:
         print(e)
