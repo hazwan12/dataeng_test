1f212bff32234104719681c0be3be06d214b0353
hazwan12 <36067775+hazwan12@users.noreply.github.com>
2020-09-13 17:25:57 +0800

    Comments and Price Above 100 Function

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pandas
 
+# Requirement : Remove any zeros prepended to the price field
+# Pandas will automatically infer datatypes and cast price field as float
 def read_file(file_name : str, file_sep : str = ",", file_header : int = 0):
     print("Accepted Parameters for file : ")
     print("Reading file {}".format(file_name))
@@ -14,18 +16,29 @@ def read_file(file_name : str, file_sep : str = ",", file_header : int = 0):
     print("File read successfully with counts {}".format(len(df)))
     return df
 
+# Requirement : Split the name field into first_name, and last_name
+# Using pandas, split name col on with " " seperator
 def split_name(df : pandas.DataFrame):
     if "name" in df.columns:
-        df[["firstname", "lastname"]] = df["name"].str.split(pat = " ", n = 1, expand = True)
+        df[["first_name", "last_name"]] = df["name"].str.split(pat = " ", n = 1, expand = True)
         return df
     else:
         raise("Column 'name' not in dataframe")
 
+# Requirement : Delete any rows which do not have a name
+# Filter dataframe where len of name is > 0
 def filter_name(df : pandas.DataFrame):
     if "name" in df.columns:
         return df[(df["name"].str.len() > 0)]
     else:
         raise("Column 'name' not in dataframe")
+
+# Requirement : Create a new field named above_100, which is true if the price is strictly greater than 100
+# 
+def price_above100(df : pandas.DataFrame):
+    df["above_100"] = df["price"] > 100
+
+    return df
 
 if __name__ == "__main__" :
     try:
@@ -37,5 +50,9 @@ if __name__ == "__main__" :
 
         df = split_name(df)
         print(df)
+
+        df = price_above100(df)
+        print(df)
+
     except Exception as e:
         print(e)
